43bdcfd0cc40fae769cb8175ad11bb13c4448c99
Chen-Yu Pao <windperson@gmail.com>
2020-05-27 13:15:51 +0800

    Add a concise way to provide bingo input data

--- BingoDemoConsoleApp/Program.cs
+++ BingoDemoConsoleApp/Program.cs
@@ -11,15 +11,17 @@ namespace BingoDemoConsoleApp
         {
             Console.WriteLine("\r\nTest Bingo game......");
 
-            var prizes = _horizontalPrize.Concat(_verticalPrize).Concat(_diagonalPrize).ToList();
+            var prizes = HorizontalPrize.Concat(VerticalPrize).Concat(DiagonalPrize).ToList();
             var bingo = new Bingo2dPrizeClincher(prizes);
 
-            var input = new List<MarkPoint2D>
-            {
-                new MarkPoint2D((0, 0)), new MarkPoint2D((0, 1)), new MarkPoint2D((0, 2)), new MarkPoint2D((0, 3)),
-                new MarkPoint2D((1, 1)), new MarkPoint2D((2, 2)), new MarkPoint2D((3, 3)),
-                new MarkPoint2D((1, 0)), new MarkPoint2D((2, 0)), new MarkPoint2D((3, 0)),
-            };
+            //actually you can provide only the points that marked true.
+            var input = new List<MarkPoint2D>()
+                .AddMarkPoint2Ds(
+                    (0, 0, true ), (0, 1, true ), (0, 2, true ), (0, 3, true ),
+                    (1, 0, true ), (1, 1, true ), (1, 2, false), (1, 3, false),
+                    (2, 0, true ), (2, 1, false), (2, 2, true ), (2, 3, false),
+                    (3, 0, true ), (3, 1, false), (3, 2, false), (3, 3, true )
+                );
 
             var matchedAward = bingo.Decide(input);
 
@@ -27,7 +29,7 @@ namespace BingoDemoConsoleApp
             Console.WriteLine(string.Join(", ", matchedAward));
         }
 
-        private static List<PrizeLine2D> _horizontalPrize => new List<PrizeLine2D>
+        private static List<PrizeLine2D> HorizontalPrize => new List<PrizeLine2D>
         {
             new PrizeLine2D(new List<(int X, int Y)> {(0, 0), (1, 0), (2, 0), (3, 0)}, "Horizontal Line1"),
             new PrizeLine2D(new List<(int X, int Y)> {(0, 1), (1, 1), (2, 1), (3, 1)}, "Horizontal Line2"),
@@ -35,7 +37,7 @@ namespace BingoDemoConsoleApp
             new PrizeLine2D(new List<(int X, int Y)> {(0, 3), (1, 3), (2, 3), (3, 3)}, "Horizontal Line4"),
         };
 
-        private static List<PrizeLine2D> _verticalPrize => new List<PrizeLine2D>
+        private static List<PrizeLine2D> VerticalPrize => new List<PrizeLine2D>
         {
             new PrizeLine2D(new List<(int X, int Y)> {(0, 0), (0, 1), (0, 2), (0, 3)}, "Vertical Line1"),
             new PrizeLine2D(new List<(int X, int Y)> {(1, 0), (1, 1), (1, 2), (1, 3)}, "Vertical Line2"),
@@ -43,7 +45,7 @@ namespace BingoDemoConsoleApp
             new PrizeLine2D(new List<(int X, int Y)> {(3, 0), (3, 1), (3, 2), (3, 3)}, "Vertical Line4"),
         };
 
-        private static List<PrizeLine2D> _diagonalPrize => new List<PrizeLine2D>
+        private static List<PrizeLine2D> DiagonalPrize => new List<PrizeLine2D>
         {
             new PrizeLine2D(new List<(int X, int Y)> {(0, 0), (1, 1), (2, 2), (3, 3)}, "Diagonal Line1"),
             new PrizeLine2D(new List<(int X, int Y)> {(3, 0), (2, 1), (1, 2), (0, 3)}, "Diagonal Line2"),
